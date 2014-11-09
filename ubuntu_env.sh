7ebb68295ea1141aa12f4c6eeebed711549ec56f
Guilherme Pontes <guilhermepontes@msn.com>
2014-11-09 21:44:18 -0200

    removing sudo :neutral_face:

--- ubuntu_env.sh
+++ ubuntu_env.sh
@@ -1,19 +1,19 @@
 echo -e '⇢  Installing all the Linux shit, it may need your password! (-;'
 
 #primary shit
-sudo apt-get install curl git mercurial make binutils bison gcc build-essential
+apt-get install curl git mercurial make binutils bison gcc build-essential
 
 #normal shit
-sudo apt-get install -y ack-grep
-sudo apt-get install -y chromium-browser
-sudo apt-get install -y python-dev
-sudo apt-get install -y python-setuptools
-sudo apt-get install -y sublime-text
-sudo apt-get install -y vim
-sudo apt-get install -y tmux
+apt-get install -y ack-grep
+apt-get install -y chromium-browser
+apt-get install -y python-dev
+apt-get install -y python-setuptools
+apt-get install -y sublime-text
+apt-get install -y vim
+apt-get install -y tmux
 
 #node
-sudo apt-get install nodejs
+apt-get install nodejs
 
 #gvm && go
 bash < <(curl -sSL https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
@@ -22,48 +22,48 @@ gvm install go1.2
 gvm use go1.2 --default
 
 #rvm && ruby
-sudo curl -sSL https://get.rvm.io | bash -s stable
-sudo echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"' >> $HOME/.bash_profile
-sudo rvm install 1.9.2
-sudo rvm install 1.9.3
-sudo rvm install 2.1.3 
+curl -sSL https://get.rvm.io | bash -s stable
+echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"' >> $HOME/.bash_profile
+rvm install 1.9.2
+rvm install 1.9.3
+rvm install 2.1.3
 rvm install ruby --latest #stable
-sudo apt-get install rake rubygems irb rdoc 
-sudo gem install rails
-sudo gem install sinatra
-sudo gem install padrino
+apt-get install rake rubygems irb rdoc
+gem install rails
+gem install sinatra
+gem install padrino
 
 #npm
-sudo bash < < (curl http://npmjs.org/install.sh | sh)
+bash < < (curl http://npmjs.org/install.sh | sh)
 
 #npm shit
-sudo npm install -g n
-sudo npm install -g spot
-sudo npm install -g hicat 
-sudo npm install -g gulp
-sudo npm install -g grunt-cli
-sudo npm install -g coffee-script
-sudo npm install -g express
-sudo npm install -g koa
-sudo npm install -g serve
-sudo npm install -g yo
-sudo npm install -g browserify
-sudo npm install -g plato
-sudo npm install -g jshint
-sudo npm install -g nodemon
-sudo npm install -g uglify-js
-sudp npm install -g commander
+npm install -g n
+npm install -g spot
+npm install -g hicat
+npm install -g gulp
+npm install -g grunt-cli
+npm install -g coffee-script
+npm install -g express
+npm install -g koa
+npm install -g serve
+npm install -g yo
+npm install -g browserify
+npm install -g plato
+npm install -g jshint
+npm install -g nodemon
+npm install -g uglify-js
+npm install -g commander
 
 #git extras
-(cd /tmp && git clone --depth 1 https://github.com/visionmedia/git-extras.git && cd git-extras && sudo make install)
+(cd /tmp && git clone --depth 1 https://github.com/visionmedia/git-extras.git && cd git-extras && make install)
 
 #python shit
 easy_install pip
 pip install virtualenvwrapper
 
 #vlc
-sudo apt-get update
-sudo apt-get install vlc browser-plugin-vlc
+apt-get update
+apt-get install vlc browser-plugin-vlc
 
 #vim janus
 curl -Lo- https://bit.ly/janus-bootstrap | bash
