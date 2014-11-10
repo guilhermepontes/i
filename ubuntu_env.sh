f24323e17af368bce2ec5f5e0fe10dba7e9f39b8
Guilherme Pontes <guilhermepontes@msn.com>
2014-11-09 22:00:22 -0200

    all the ubuntu env shit

--- ubuntu_env.sh
+++ ubuntu_env.sh
@@ -1,69 +1,27 @@
-echo -e '⇢  Installing all the Linux shit, it may need your password! (-;'
+echo -e '\e[1m⇢  Installing all the \e[34mLinux\e[39m shit!\e[0m'
 
-#primary shit
+# primary shit
 apt-get install curl git mercurial make binutils bison gcc build-essential
-
-#normal shit
 apt-get install -y ack-grep
+
+# fucking browser
 apt-get install -y chromium-browser
-apt-get install -y python-dev
-apt-get install -y python-setuptools
+
+# text editors
 apt-get install -y sublime-text
 apt-get install -y vim
 apt-get install -y tmux
 
-#node
+# python
+apt-get install -y python-dev
+apt-get install -y python-setuptools
+
+# ruby
+apt-get install rake rubygems irb rdoc
+
+# node
 apt-get install nodejs
 
-#gvm && go
-bash < <(curl -sSL https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
-source $HOME/.gvm/scripts/gvm
-gvm install go1.2
-gvm use go1.2 --default
-
-#rvm && ruby
-curl -sSL https://get.rvm.io | bash -s stable
-echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"' >> $HOME/.bash_profile
-rvm install 1.9.2
-rvm install 1.9.3
-rvm install 2.1.3
-rvm install ruby --latest #stable
-apt-get install rake rubygems irb rdoc
-gem install rails
-gem install sinatra
-gem install padrino
-
-#npm
-bash < < (curl http://npmjs.org/install.sh | sh)
-
-#npm shit
-npm install -g n
-npm install -g spot
-npm install -g hicat
-npm install -g gulp
-npm install -g grunt-cli
-npm install -g coffee-script
-npm install -g express
-npm install -g koa
-npm install -g serve
-npm install -g yo
-npm install -g browserify
-npm install -g plato
-npm install -g jshint
-npm install -g nodemon
-npm install -g uglify-js
-npm install -g commander
-
-#git extras
-(cd /tmp && git clone --depth 1 https://github.com/visionmedia/git-extras.git && cd git-extras && make install)
-
-#python shit
-easy_install pip
-pip install virtualenvwrapper
-
-#vlc
+# vlc to watch some shit
 apt-get update
 apt-get install vlc browser-plugin-vlc
-
-#vim janus
-curl -Lo- https://bit.ly/janus-bootstrap | bash
