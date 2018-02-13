5b7cb562e5603f94784757b2edf844a96bcc399d
Guilherme Pontes <guilhermepontes@msn.com>
2018-02-13 16:57:25 +0100

    Update common.sh

--- common.sh
+++ common.sh
@@ -3,14 +3,12 @@ echo -e '⇢  Installing all the [common] shit!'
 # gvm && go
 curl -sSL https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer | bash
 source $HOME/.gvm/scripts/gvm
-gvm install go1.2
-gvm use go1.2 --default
+# pick version
+# gvm install go1.2
+# gvm use go1.2 --default
 
 # rvm && ruby
 curl -sSL https://get.rvm.io | bash -s stable
-rvm install 1.9.2
-rvm install 1.9.3
-rvm install 2.1.3
 rvm install ruby --latest #stable
 gem install rails
 gem install sinatra
@@ -19,22 +17,22 @@ gem install sinatra
 curl -sSl http://npmjs.org/install.sh | sh
 
 # npm shit
-npm install -g n
-npm install -g spot
-npm install -g hicat
-npm install -g gulp
-npm install -g grunt-cli
-npm install -g coffee-script
-npm install -g express
-npm install -g koa
-npm install -g serve
-npm install -g yo
-npm install -g browserify
-npm install -g plato
-npm install -g jshint
-npm install -g nodemon
-npm install -g uglify-js
-npm install -g commander
+npm i -g n
+npm i -g npx
+npm i -g yarn
+npm i -g spot
+npm i -g hicat
+npm i -g express
+npm i -g serve
+npm i -g yo
+npm i -g to
+npm i -g jshint
+npm i -g nodemon
+npm i -g brb
+npm i -g gtop
+npm i -g react-native-cli
+npm i -g jscodeshift
+npm i -g serverless
 
 # git
 (cd /tmp && git clone --depth 1 https://github.com/visionmedia/git-extras.git && cd git-extras && make install)
